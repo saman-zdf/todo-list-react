411782bab7a6f16284d9241f56f18cbca176e575
saman-zdf <samanfyz68@gmail.com>
2021-10-14 07:14:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+  return <h1>Hello React</h1>;
+};
 
-export default App
+export default App;
